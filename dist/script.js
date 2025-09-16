429297a2fcea59b555a800713a182455f2411f79
ThanhTran <tthanhs678@gmail.com>
2025-09-16 20:12:18 +0700

    1

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "Alice";
+let age = 30;
+console.log(`Name: ${name}, Age: ${age}`);
---
+++ script.ts
@@ -0,0 +1,23 @@
+// let name: string = "Alice";
+// let age: number = 30;
+// console.log(`Name: ${name}, Age: ${age}`);
+// Tam giác
+let a: number = 400;
+let b: number = 350;
+let c: number = 200;
+let d = a + b + c;
+console.log("Chu vi tam giác:", d);
+
+// Chu vi hình chữ nhật
+let L: number = 20;
+let R: number = 30;
+let P = (L + R) * 2;
+console.log("Chu vi hình chữ nhật:", P);
+
+// Diện tích hình tròn
+let r: number = 10;
+let S = Math.PI * r * r;
+console.log("Diện tích hình tròn:", S);
+
+
+
