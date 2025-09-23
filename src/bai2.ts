f99e3fca2221dd94103689fd0a1bc688793466a7
ThanhTran <tthanhs678@gmail.com>
2025-09-23 19:05:57 +0700

    3

--- src/bai2.ts
+++ src/bai2.ts
@@ -3,4 +3,13 @@ function dayso(str: string, char: string): number {
 }
 console.log("số lần xuất hiện",dayso("hello world", "o")); // o xuất hiện trong hello world 2 lần 
 
-console.log("số lần xuất hiện",dayso("hello world", "l")); // l xuất hiện 3 lần 
+console.log("số lần xuất hiện",dayso("hello world", "L")); // l xuất hiện 3 lần 
+
+
+//split là một hàm có sẵn của kiểu chuỗi (string) trong JavaScript/TypeScript. 
+// Nó dùng để cắt chuỗi thành một mảng con dựa trên dấu phân tách (separator) mà bạn chỉ định.
+
+//string.split(separator, limit?)
+//separator: ký tự hoặc chuỗi để cắt (ví dụ " ", "o", "," …).1
+
+//limit (tuỳ chọn): số lượng phần tử tối đa trong mảng kết quả.
---
+++ src/function.ts
@@ -0,0 +1,6 @@
+// const hobbies = ["Thể thao", "Du lịch"];
+// const activeHobby = ['Du lịch'];
+// activeHobby.push(hobbies);
+// activeHobby.push(hobbies[0],hobbies[1]);
+// activeHobby.push(...hobbies);
+// console.log(activeHobby);
